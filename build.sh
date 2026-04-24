5fc8caceae2bc8318bb0ee0fab48cb9b0845f6fd
Nor-Ain <ainsampiano@gmail.com>
2026-04-24 23:37:31 +0800

    Add migrations to build script

--- build.sh
+++ build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-echo "Building ReporTrash for Render..."
-
 # Install dependencies
 pip install -r requirements.txt
 
-# Collect static files
-python manage.py collectstatic --noinput --settings=barangay_waste.render_settings
-
 # Run migrations
-python manage.py migrate --noinput --settings=barangay_waste.render_settings
+python manage.py migrate --noinput
 
-echo "Build completed!"
+# Collect static files
+python manage.py collectstatic --noinput
+
+echo "Build completed successfully!"
