97c97f34f0288ac2615549737411ba53953df2f8
Nor-Ain <ainsampiano@gmail.com>
2026-04-25 00:06:02 +0800

    Add migration endpoint URL pattern

--- barangay_waste/urls.py
+++ barangay_waste/urls.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from waste_management import views
+from waste_management.views import migrate_database 
 
 urlpatterns = [
+    path('migrate/', migrate_database, name='migrate'),
+
     path('admin/', admin.site.urls),
     path('', include('waste_management.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
